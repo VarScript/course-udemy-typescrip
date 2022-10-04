1be277d91b548b87f4bd4cfc2ee7dc745358d06b
DevHars <vargassanchezfabio01@gmail.com>
2022-10-04 16:46:29 -0500

    interfere types and strict mode

---
+++ 02-basic-types/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Anonime fucntion auto invoked
+(() => {
+    const a = 10;
+    console.log(a);
+})();
---
+++ 02-basic-types/app.ts
@@ -0,0 +1,6 @@
+// Anonime fucntion auto invoked
+(() => {
+    const a:number = 10;
+    console.log(a);
+})();
+
