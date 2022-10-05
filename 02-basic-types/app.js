8720469f3ceca1ff430c417a0bcd7eb6a24d83c9
DevHars <vargassanchezfabio01@gmail.com>
2022-10-05 17:38:24 -0500

    task

--- 02-basic-types/app.js
+++ 02-basic-types/app.js
@@ -1,6 +1,35 @@
 "use strict";
-// Anonime fucntion auto invoked
 (() => {
-    const a = 10;
-    console.log(a);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let force;
+    (function (force) {
+        force[force["Acuaman"] = 0] = "Acuaman";
+        force[force["Batman"] = 1] = "Batman";
+        force[force["Flash"] = 5] = "Flash";
+        force[force["Superman"] = 100] = "Superman";
+    })(force || (force = {}));
+    const fuerzaSuperman = force.Superman;
+    const fuerzaBatman = force.Batman;
+    const fuerzaFlash = force.Flash;
+    const fuerzaAcuaman = force.Acuaman;
+    // Retorno de funciones
+    const activar_batiseñal = () => {
+        return 'activada';
+    };
+    const pedir_ayuda = () => {
+        console.log('Auxilio!!!');
+    };
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
--- 02-basic-types/app.ts
+++ 02-basic-types/app.ts
@@ -1,6 +1,45 @@
-// Anonime fucntion auto invoked
 (() => {
-    const a:number = 10;
-    console.log(a);
-})();
 
+    // Tipos
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
+  
+    const existe:boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+
+    enum force {
+      Acuaman = 0,
+      Batman = 1,
+      Flash = 5,
+      Superman = 100,
+    }
+    const fuerzaSuperman:force = force.Superman;
+    const fuerzaBatman:force = force.Batman;
+    const fuerzaFlash:force = force.Flash;
+    const fuerzaAcuaman:force = force.Acuaman;
+    
+    // Retorno de funciones
+    const activar_batiseñal = ():string => {
+      return 'activada';
+    }
+  
+    const pedir_ayuda = ():void => {
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  })()
+  
+  
