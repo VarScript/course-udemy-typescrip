edbd11ac895d7a0774d7b5a873da33f830d9b5d8
DevHars <vargassanchezfabio01@gmail.com>
2022-10-07 19:27:59 -0500

    basic objects

---
+++ 02-basic-types/objects/objects.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    let flash = {
+        name: 'Barry',
+        age: 24,
+        powers: ['Super velocity', 'Travel in the time']
+    }
+
+    flash = {
+        name: 'Clark Clein',
+        age: 63,
+        powers: ['Super strong'],
+    }
+})()
