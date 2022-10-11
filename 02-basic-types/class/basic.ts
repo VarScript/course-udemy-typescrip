8fc42bac892266053474877535d420a2e9730004
DevHars <vargassanchezfabio01@gmail.com>
2022-10-11 16:29:55 -0500

    private and public methods

--- 02-basic-types/class/basic.ts
+++ 02-basic-types/class/basic.ts
@@ -5,16 +5,29 @@
         // public team: string;
         // public realName?: string;
         static avgAge: number = 35;
+        static getAvgAge() {
+            return this.name;
+        }
 
         constructor( 
             private name:string, 
             private team:string, 
             public realName?:string
         ) {}
+
+        public bio() {
+            return `${this.name} (${this.team})`
+        }
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Cap', 'Scott Lang');
+    const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log( antman );
+
+    console.log(antman.bio());
+
+    console.log(Avenger.getAvgAge());
+    
+    
     
     console.log(Avenger.avgAge);
 })()
--- 02-basic-types/main.js
+++ 02-basic-types/main.js
@@ -6,10 +6,18 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'Cap', 'Scott Lang');
+    const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
     console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
