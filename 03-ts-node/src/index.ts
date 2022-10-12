dc95015e14775dd23f88a288a8c092a8b2c08815
DevHars <vargassanchezfabio01@gmail.com>
2022-10-12 16:30:14 -0500

    generics

--- 03-ts-node/src/index.ts
+++ 03-ts-node/src/index.ts
@@ -1,18 +1,3 @@
-// as is an alias of Hero
-// import { Hero as superHero} from './classes/Hero';
-// const ironman = new superHero('Ironman', 100, 55);
-
-// Access of all of the export of that file
-// import * as HeroClasses from './classes/Hero';
-// const ironman = new HeroClasses.Hero('Ironman', 100, 55);
+import { printObject } from './generics/generics';
 
 
-// import powers, { Power } from './data/powers';
-
-
-import { Hero } from './classes/Hero';
-
-const ironman = new Hero('Ironman', 1, 55);
-
-console.log(ironman);
-console.log(ironman.power);
