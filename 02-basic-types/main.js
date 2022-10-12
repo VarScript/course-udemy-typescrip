40296bc79a621a409aaa2bccd91848e050b41c2d
DevHars <vargassanchezfabio01@gmail.com>
2022-10-12 15:30:21 -0500

    namespace created

--- 02-basic-types/main.js
+++ 02-basic-types/main.js
@@ -1,55 +1,14 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry',
-        age: 24,
-        powers: [12]
+var Validations;
+(function (Validations) {
+    Validations.vaidateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Clen',
-        age: 24,
-        powers: [1],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
     };
-})();
-(() => {
-    class Mutand {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutanPower(id) {
-            return `${this.name} ${this.realName}`;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Fabio',
-        age: 21,
-        address: {
-            id: 120,
-            zip: 'HS6',
-            city: 'Roma'
-        }
-    };
-    const client2 = {
-        name: 'Any',
-        age: 24,
-        address: {
-            id: 140,
-            zip: 'HS6',
-            city: 'China'
-        }
-    };
-})();
-(() => {
-    let sum;
-    sum = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.vaidateText('Fab'));
 //# sourceMappingURL=main.js.map
