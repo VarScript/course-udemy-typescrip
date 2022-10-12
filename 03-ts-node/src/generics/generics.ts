2187012c23631ee0dbd14a6e91ee42e786644c64
DevHars <vargassanchezfabio01@gmail.com>
2022-10-12 16:42:41 -0500

    function generic

--- 03-ts-node/src/generics/generics.ts
+++ 03-ts-node/src/generics/generics.ts
@@ -1,4 +1,10 @@
 export const printObject = (argument:any) => {
     console.log(argument);
-    
 }
+
+// The standard for the generic is the letter T but it is may use letter or word
+export function genericFunction<T>( argument:T ):T {
+    return argument;
+}
+
+export const genericFunctionArrow = <T>( argument:T ):T => argument;
--- 03-ts-node/src/index.ts
+++ 03-ts-node/src/index.ts
@@ -1,3 +1,9 @@
-import { printObject } from './generics/generics';
+import { printObject, genericFunction, genericFunctionArrow } from './generics/generics';
+
+
+const name:string = 'Fabio';
+console.log( genericFunctionArrow( 3.1416 ).toFixed(2) );
+console.log( genericFunctionArrow( name ).toUpperCase() );
+console.log( genericFunctionArrow( new Date ).getDate() );
 
 
