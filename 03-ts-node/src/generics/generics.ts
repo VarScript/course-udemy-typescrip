2fc531df3a65137072cc81c4db0d4a497c9714bf
DevHars <vargassanchezfabio01@gmail.com>
2022-10-12 16:54:45 -0500

    function generic in action

--- 03-ts-node/src/generics/generics.ts
+++ 03-ts-node/src/generics/generics.ts
@@ -7,4 +7,6 @@ export function genericFunction<T>( argument:T ):T {
     return argument;
 }
 
+
+
 export const genericFunctionArrow = <T>( argument:T ):T => argument;
--- 03-ts-node/src/index.ts
+++ 03-ts-node/src/index.ts
@@ -1,9 +1,18 @@
 import { printObject, genericFunction, genericFunctionArrow } from './generics/generics';
+import { Hero } from './interfaces/hero';
+import { Villain } from './interfaces/villain';
+
+// const name:string = 'Fabio';
+// console.log( genericFunctionArrow( 3.1416 ).toFixed(2) );
+// console.log( genericFunctionArrow( name ).toUpperCase() );
+// console.log( genericFunctionArrow( new Date ).getDate() );
 
 
-const name:string = 'Fabio';
-console.log( genericFunctionArrow( 3.1416 ).toFixed(2) );
-console.log( genericFunctionArrow( name ).toUpperCase() );
-console.log( genericFunctionArrow( new Date ).getDate() );
+const deadpool = {
+    name: 'Deadpoll',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130
+}
 
+console.log( genericFunctionArrow<Villain>( deadpool ).dangerLevel );
 
