baf246eb6bccec9419430d42b8af4f574fe0ae6a
DevHars <vargassanchezfabio01@gmail.com>
2022-10-14 20:22:01 -0500

    express with typescript

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,13 +1,20 @@
-const express = require('express')
+import express from 'express';
+// const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    res.status(401).json({
+        ok: false,
+        msg: 'There not token in the petition'
+    })
     res.json({
         ok: true,
         msg: 'All came good!'
     })
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
